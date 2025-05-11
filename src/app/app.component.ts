5bd1089bb091e4744e56d7adeddbcc66058a71c3
ckab04 <ciceronkapenabm@gmail.com>
2025-05-11 21:49:38 +0200

    Simple test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'cypress_jenkins';
+  title = "cypress_jenkins";
+
+  onButtonClick() {
+    console.log("The button has been clicked");
+  }
+
+  submit() {
+    console.log("The form has been submitted");
+  }
 }
