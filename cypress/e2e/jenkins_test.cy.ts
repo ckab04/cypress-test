8b77f712ac43d6178ea10091bf2d46ef75a3bf61
ckab04 <ciceronkapenabm@gmail.com>
2025-05-12 01:04:11 +0200

    Ajout du second test

--- cypress/e2e/jenkins_test.cy.ts
+++ cypress/e2e/jenkins_test.cy.ts
@@ -1,7 +1,13 @@
 describe("Test Jenkins", () => {
-  it("passes", () => {
+  it("Premier test", () => {
     //cy.visit("http://localhost:4200/");
     cy.log("Ceci est un simple test de cypress et Jenkins");
+    //cy.get("h1").contains("Hello");
+  });
+
+  it("Second test", () => {
+    cy.visit("http://www.google.com");
+    cy.log("Ceci est le second test");
     //cy.get("h1").contains("Hello");
   });
 });
